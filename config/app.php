29dc2398f683a8de0dcc436b8cc1ac04f37aee1d
AntolinoRetana <94326680+AntolinoRetana@users.noreply.github.com>
2026-06-13 10:49:29 -0600

    :art: feat(config): actualizar el nombre de la aplicación a 'SIG C.E. Pablo J. Aguirre'

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'SIG C.E. Pablo J. Aguirre'),
 
     /*
     |--------------------------------------------------------------------------
